232b104158804f7d021756740d522998d6923df9
wonsokoh1019 <56017035+wonsokoh1019@users.noreply.github.com>
2019-11-15 22:38:01 +0900

    Update config.py
    LOCAL ADDRESS update

--- conf/config.py
+++ conf/config.py
@@ -15,7 +15,7 @@ SERVER_ID = "f7cecbb969b74579b90390406412108f"
 PRIVATE_KEY_NAME = "private_20191115210109.key"
 
 # LOCAL ADDRESS
-LOCAL_ADDRESS = "https://10.105.180.133:8080/"
+LOCAL_ADDRESS = "https://samplebot-app.herokuapp.com/ "
 
 # DB config
 DB_HOST = "ec2-54-235-180-123.compute-1.amazonaws.com"
